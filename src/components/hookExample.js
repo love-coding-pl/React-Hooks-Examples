9ed1f0f609a0071d84799e8449c6834eb2e47f66
JoannaLigenza <ligenzajoanna@gmail.com>
2019-11-19 18:51:16 +0100

    useReducer hook - set state in one function with switch options

--- src/components/hookExample.js
+++ src/components/hookExample.js
@@ -8,12 +8,18 @@ const HookExample = () => {
                 return {count: state.count + 1};
             case 'substract':
                 return {count: state.count - 1};
+            case "reset":
+                return action.setInitState;
             default:
-                return {count: state}
+                return {count: state};
         }
     }
 
-    // state - Declaring new state variable, called "counter"
+    const init = () => {
+        return {count: 0}
+    }
+
+    // state - Declaring new state variable, called "counter" and initial count state
     const [counter, setCounter] = useReducer(switchOption, {
         count: 0
     });
@@ -22,6 +28,9 @@ const HookExample = () => {
       <p> Counter: {counter.count} </p>
       <button onClick={() => setCounter({type: "add"})} > +1 </button>
       <button onClick={() => setCounter({type: "substract"})} > -1 </button>
+      <div>
+         <button onClick={() => setCounter({type: "reset", setInitState: init()})} > reset </button>
+      </div>
     </div>
   );
 }
